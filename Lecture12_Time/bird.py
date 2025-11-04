1d590d6654c8b97a55d31a3cf67573c7e1a8e542
Kwon jeonghyeok <jhkwon036@gmail.com>
2025-11-04 23:15:30 +0900

    랜덤한 위치에 새 10마리 그리기만 구현

--- Lecture12_Time/bird.py
+++ Lecture12_Time/bird.py
@@ -1,7 +1,12 @@
 from pico2d import load_image
+import random
 
 class Bird:
     def __init__(self):
+        self.image = load_image('bird_animation.png')
+        self.x, self.y = random.randint(100,1500), random.randint(400,550)
+        self.frame = 0
+        self.dir = 1
         pass
 
 
@@ -10,4 +15,5 @@ class Bird:
         pass
 
     def draw(self):
+        self.image.clip_draw(0, 182, 182, 166, self.x, self.y, 91, 83)
         pass
